e8bec04377010c34dbbec4a3dea684a25385718d
basilmariano <basil@clicksend.com>
2020-11-21 13:17:51 +0800

    Initial Commit

---
+++ HaKiosk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaKiosk
+//
+//  Created by ClickSend on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
